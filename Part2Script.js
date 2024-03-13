de40fc09e7d045e6b4d80fc35cf7da04f6a313e5
Josue Morales <157775085+PR2014FL@users.noreply.github.com>
2024-03-13 19:55:55 -0400

    Part 2 completed.

--- Part2Script.js
+++ Part2Script.js
@@ -7,6 +7,21 @@ const fuelEfficiency = {
 const fuelBudget = 175;
 const avgCostGallon = 3;
 const totalGallons = fuelBudget/avgCostGallon;
-const fuelNeeded55 = tripDistance/fuelEfficiency[55];
+const fuelNeeded55 = tripDistance/fuelEfficiency[55];//1500/30 = 50 gallons
 const fuelNeeded60 = tripDistance/fuelEfficiency[60];
 const fuelNeeded75 = tripDistance/fuelEfficiency[75];
+
+//How many gallons of fuel will you need for the entire trip?
+console.log(`How many gallons of fuel will you need for the entire trip if you average at 55MPH? ${fuelNeeded55} gallons.`)
+console.log(`How many gallons of fuel will you need for the entire trip if you average at 60MPH? ${fuelNeeded60} gallons.`)
+console.log(`How many gallons of fuel will you need for the entire trip if you average at 75MPH? ${fuelNeeded75} gallons.`)
+
+//Will your budget be enough?
+console.log(`Your budget of $175 will be enough for the entire trip traveling at 50MPH? ${(fuelNeeded55 * 3) < 175}`);
+console.log(`Your budget of $175 will be enough for the entire trip traveling at 60MPH? ${(fuelNeeded60 * 3) < 175}`);
+console.log(`Your budget of $175 will be enough for the entire trip traveling at 75MPH? ${(fuelNeeded75 * 3) < 175}`);
+
+//How long will the trip take, in hours?
+console.log(`How long will the trip take, in hours at 55MPH? ${tripDistance / 55} hours.`);
+console.log(`How long will the trip take, in hours at 60MPH? ${tripDistance / 60} hours.`);
+console.log(`How long will the trip take, in hours at 75MPH? ${tripDistance / 75} hours.`);
